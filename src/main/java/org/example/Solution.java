d4b13783a14d87769f7859c599b2e888eabf64b4
Jovan Bojovic <jovan.bojovic@comtrade.com>
2022-09-20 16:34:48 +0200

    Created method for matching word inside word

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Solution {
 
-    public int solution(String s) {
+    private int[] countBalloonLetters(String s) {
         int array[] = new int[5];
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == 'B')
@@ -16,7 +16,10 @@ public class Solution {
             if(s.charAt(i) == 'N')
                 array[4]++;
         }
+        return array;
+    }
 
+    private int ballonCharOccurance(int array[]) {
         int counter = 0;
         while (array[0] > 0 && array[1] > 0 && array[2] > 1 && array[3] > 1 && array[4] > 0) {
             array[0]--;
@@ -27,6 +30,34 @@ public class Solution {
             counter++;
         }
         return counter;
+    }
+
+    public int solution(String s) {
+        return ballonCharOccurance(countBalloonLetters(s));
+    }
+
+    private int[] getCharsFromString(String s) {
+        int array[] = new int[256];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            array[(int) s.charAt(i)]++;
+        }
+        return array;
+    }
+
+    public int twoWordsSolution(String wordFromChar, String word) {
+        int firstWordChar[] = getCharsFromString(wordFromChar);
+        int secondWordChar[] = getCharsFromString(word);
+
+        int count = Integer.MAX_VALUE;
+        int wordLength = word.length();
+        for (int i = 0; i < wordLength; i++) {
+            if (secondWordChar[(int) word.charAt(i)] != 0) {
+                int currentLetterAccurance = firstWordChar[(int) word.charAt(i)] / secondWordChar[(int) word.charAt(i)];
+                count = count > currentLetterAccurance ? currentLetterAccurance : count;
+            }
+        }
+        return count;
     }
 
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -2,16 +2,14 @@ import org.example.Solution;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
-public class Tests {
+public class SolutionTest {
 
     @InjectMocks
     private Solution solution = new Solution();
@@ -32,6 +30,13 @@ public class Tests {
         assertEquals(0, solution.solution("0186031"));
         assertEquals(1, solution.solution("BAONX421XO1LL"));
         assertEquals(2, solution.solution("BAOOLLNNOLOLGBAX"));
+    }
+
+    @Test
+    public void twoWordSolutionTest() {
+        assertEquals(2, solution.twoWordsSolution("cssatllacggt", "cat"));
+        assertEquals(0, solution.twoWordsSolution("123411", "abc"));
+        assertEquals(2, solution.twoWordsSolution("abcabc", "abc"));
     }
 
 }
